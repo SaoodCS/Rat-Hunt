23b249c0e78739ac40e5c875916455e4d11b5dca
Saood Aslam <saood-aslam@outlook.com>
2024-02-03 15:31:35 +0000

    fonts added and updated logo font

--- frontend/src/global/components/app/logo/LogoText.tsx
+++ frontend/src/global/components/app/logo/LogoText.tsx
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 export const LogoText = styled.div<{ color?: string; size?: string }>`
    font-size: ${({ size }) => size || '8em'};
    color: ${({ color }) => color || '#6C32D1'};
-   font-family: 'Stencil Std', fantasy;
-   letter-spacing: 3px;
+   font-family: 'Backfired';
+   @font-face {
+      font-family: 'Backfired';
+      src: url('/fonts/Backfired.ttf') format('truetype');
+   }
 `;
