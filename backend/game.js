876f12f891cfb235c72d66d8ca0b13b18229a1cc
SaoodCS <saood-aslam@outlook.com>
2024-02-07 12:14:34 +0000

    lint config and fixes for the backend

--- backend/game.js
+++ backend/game.js
@@ -1,31 +1,30 @@
-const topics = require("./topics");
-const dbLogic = require("./db_interaction")
+const topics = require('./topics');
 
 // Function to get a random grid
 function getRandomTopic() {
-    return topics[getRandomChoice(topics.length)];
+   return topics[getRandomChoice(topics.length)];
 }
 
 // Get 6 random words based on the key
 function getRandomWords(key) {
-  const topic = topics.find(t => t.key === key);
+   const topic = topics.find((t) => t.key === key);
 
-  if (topic && topic.values && topic.values.length >= 6) {
+   if (topic && topic.values && topic.values.length >= 6) {
       const shuffledWords = topic.values.sort(() => Math.random() - 0.5);
       return shuffledWords.slice(0, 6);
-  } else {
+   } else {
       console.error(`Unable to find or retrieve words for key: ${key}`);
       return [];
-  }
+   }
 }
 
 // Function to get a random choice from 0 to N-1
 function getRandomChoice(N) {
-    return Math.floor(Math.random() * N);
+   return Math.floor(Math.random() * N);
 }
 
 module.exports = {
-  getRandomTopic,
-  getRandomChoice,
-  getRandomWords,
+   getRandomTopic,
+   getRandomChoice,
+   getRandomWords,
 };
