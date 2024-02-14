0c9debf2a973afb99cdbc159e93b4c75c299192a
SaoodCS <saood-aslam@outlook.com>
2024-02-14 21:43:23 +0000

    Add Firestore functionality to onDataChange function

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,5 +1,7 @@
 import * as admin from "firebase-admin";
+import { FieldValue } from "firebase-admin/firestore";
 import * as functions from "firebase-functions";
+import Helpers from "./helpers/Helpers";
 
 if (!admin.apps.length) {
   admin.initializeApp();
@@ -8,16 +10,30 @@ if (!admin.apps.length) {
 export const onDataChange = functions.database
   .ref("/")
   .onWrite(async (change, context) => {
-    console.log("hello")
+    console.log("hello");
     const newValue = change.after.val();
     const original = change.before.val();
-    const pathOfChangedValue = context.resource;
-     // add these to the firestore collection "test" and document "test":
-     await admin.firestore().collection("test").doc("test").set({
-        newValue,
-        original,
-        pathOfChangedValue
-      });
-    return null;
-    
+
+    const { roomId, userId, field, value } = Helpers.getChangedValDetails(
+      original,
+      newValue
+    );
+
+    const roomRef = admin.firestore().collection("games").doc(`room-${roomId}`);
+    const roomSnapshot = await roomRef.get();
+    const roomData = roomSnapshot.data();
+    const users = roomData?.users;
+    const userIndex = users.findIndex((user: any) => user.userId === userId);
+    const user = users[userIndex];
+    await roomRef.update({
+      users: FieldValue.arrayRemove(user),
+    });
+    const fieldToUpdate = field === "lastOnline" ? "lastOnline" : "userStatus";
+    await roomRef.update({
+      users: FieldValue.arrayUnion({
+        ...user,
+        [fieldToUpdate]: value,
+      }),
+    });
   });
+
