61750dcbe0155ddc0022869bb5ea7d8ce7205338
SaoodCS <saood-aslam@outlook.com>
2024-02-20 14:28:14 +0000

    gameplay component init

---
+++ frontend/src/pages/main/startedGame/gameplay/Gameplay.tsx
@@ -0,0 +1,7 @@
+export default function Gameplay(): JSX.Element {
+   return (
+      <div>
+         <h1>Gameplay</h1>
+      </div>
+   );
+}
--- frontend/src/pages/main/startedGame/style/Style.tsx
+++ frontend/src/pages/main/startedGame/style/Style.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import Color from '../../../global/css/colors';
+import Color from '../../../../global/css/colors';
 
 export const GamePageWrapper = styled.div`
    background: linear-gradient(
