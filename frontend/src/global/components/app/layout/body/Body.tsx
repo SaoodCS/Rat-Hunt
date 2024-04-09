a611d67d74aafb2850e476126f7e7579b257d060
SaoodCS <saood-aslam@outlook.com>
2024-04-09 12:10:05 +0100

    Update background gradient in Body.tsx

--- frontend/src/global/components/app/layout/body/Body.tsx
+++ frontend/src/global/components/app/layout/body/Body.tsx
@@ -1,17 +1,11 @@
 import styled from 'styled-components';
 import MyCSS from '../../../../css/MyCSS';
-import Color from '../../../../css/colors';
 
 export const Body = styled.div<{ isDarkTheme: boolean }>`
    position: fixed;
    width: 100dvw;
    top: 10%;
    bottom: 0;
-   background: linear-gradient(
-      to bottom,
-      ${Color.setRgbOpacity(Color.darkThm.accent, 0)},
-      ${Color.setRgbOpacity(Color.darkThm.accent, 0.1)}
-   );
    @media (min-width: ${MyCSS.PortableBp.asPx}) {
       margin-top: 20px;
    }
